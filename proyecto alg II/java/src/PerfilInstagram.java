da15c65b835220f19d7c69251438d84093a86136
Bruno1224 <brunomenna2011@gmail.com>
2023-04-24 22:58:30 -0300

    clase 24

---
+++ proyecto alg II/java/src/PerfilInstagram.java
@@ -0,0 +1,15 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.*;
+
+
+public class PerfilInstagram {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
+        List<Publicacion> publicaciones = CargadorDatos.cargarPublicacionesDesdeArchivo("publicaciones.xml");
+        List<Album> albums = new ArrayList<>();
+
+    }
+}
--- proyecto alg II/java/src/Video.java
+++ proyecto alg II/java/src/Video.java
@@ -2,7 +2,9 @@ public class Video extends Publicacion{
 
     private int resolucion,duracion,cantcuadros;
 
-    //Permitir avanzar, detener, y aplicar filtros.
+    /**
+     * Permitir avanzar, detener, y aplicar filtros.
+     */
     public void reproducir() {
 
     }
