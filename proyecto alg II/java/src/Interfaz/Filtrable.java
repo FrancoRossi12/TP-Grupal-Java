9382379775ebad2f5ea295609f50000573343e8c
fededotcom <fede.alvarez03@hotmail.com>
2023-05-22 21:29:15 -0300

    AVANCE CLASE
    ESTOY HINCHADO LOS HUEVOS

--- proyecto alg II/java/src/Interfaz/Filtrable.java
+++ proyecto alg II/java/src/Interfaz/Filtrable.java
@@ -1,5 +1,7 @@
 package Interfaz;
 
 public interface Filtrable {
+
+    public int[] myIntArray = {1, 2, 3};
     void aplicarFiltro();
 }
--- proyecto alg II/java/src/TipoPublicacion/Imagen.java
+++ proyecto alg II/java/src/TipoPublicacion/Imagen.java
@@ -4,16 +4,15 @@ import Interfaz.Filtrable;
 
 public class Imagen extends Publicacion implements Filtrable {
 
-    private int resolucion, alto, ancho;
+    private int resolucion, alto, ancho, filtro;
 
     public Imagen() {
         super();
-        resolucion = alto = ancho = 5;
+        resolucion = alto = ancho = filtro = 5;
     }
 
     // metodo aplicar flitro
-    public void aplicarFiltro() {// SOLO IMAGEN Y VIDEO PUEDEN APLICAR FILTROS, LOS OTROS FILTROS SON LOS DE
-                                 // FILTRAR PUBLICACIONES
+    public void aplicarFiltro(int filtro) {// la variable int filtro es una posicion dentro del vector de filtros disponibles
 
     }
 
@@ -30,5 +29,10 @@ public class Imagen extends Publicacion implements Filtrable {
             System.out.println(".");// TOY HACIENDO PRUEBAS
         }
         System.out.println("Fin publicacion: " + this.getNombre());
+    }
+
+    @Override
+    public void aplicarFiltro() {
+
     }
 }
